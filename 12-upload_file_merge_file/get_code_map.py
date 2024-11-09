3119c9a8d5444a6ca51a2f1051623bf4a4be6022
kirito2014 <1039580151@qq.com>
2024-11-09 20:16:02 +0800

    11111

--- 12-upload_file_merge_file/get_code_map.py
+++ 12-upload_file_merge_file/get_code_map.py
@@ -24,24 +24,25 @@ def process_code_mapping(table_list_file):
     print(f"本次共处理{table_list}张表") 
     processed_table_count=0 
     
+    print(f"正在处理<{person_name}>-<{table_name}>的码值映射。") 
+    code_map_files = [ 
+        os.path.join('uploads',f'pub_cd_map-{person_name}.xlsx'), 
+        os.path.join('uploads',f'pub_cd_map-{person_name}.xls'), 
+        os.path.join('uploads',f'pub_cd_map-{person_name}.xlsm') 
+        ]
+    code_map_file = next((file for file in code_map_files if os.path.exists(file)),None)
+    print(code_map_files)
+    if not code_map_file: 
+        log_error(error_log,f"{person_name}的代码映射文件不存在.") 
+        sys.exit() 
+
     for table_name in table_names: 
-        print(f"正在处理<{person_name}>-<{table_name}>的码值映射。") 
-        code_map_files = [ 
-            os.path.join('uploads',f'pub_cd_map-{person_name}.xlsx'), 
-            os.path.join('uploads',f'pub_cd_map-{person_name}.xls'), 
-            os.path.join('uploads',f'pub_cd_map-{person_name}.xlsm') 
-            ]
-        code_map_file = next((file for file in code_map_files if os.path.exists(file)),None)
-        print(code_map_files)
-        if not code_map_file: 
-            log_error(error_log,f"{person_name}的代码映射文件不存在.") 
-            sys.exit() 
         try: 
             src_wb=xw.Book(code_map_file)
             rem_code_map_sheet='rem-代码映射' 
             if rem_code_map_sheet not in [sheet.name for sheet in src_wb.sheets]: 
                 log_error(error_log,f"{code_map_file}中未找到代码映射sheet。") 
-                #src_wb.close() 
+                src_wb.close() 
                 sys.exit()
 
             src_cm_sheet=src_wb.sheets[rem_code_map_sheet] 
@@ -63,14 +64,16 @@ def process_code_mapping(table_list_file):
                 filtered_src_cm_df=src_cm_df[src_cm_df['目标表英文名']==table_name] 
                 if filtered_src_cm_df.empty: 
                     log_error(error_log,f"{code_map_file}中未找到{table_name}表的代码映射.")
-                    continue
                 else:
                     start_row = tgt_cm_sheet.range('A1').expand('down').last_cell.row + 1
                     tgt_cm_sheet.range(f'A{start_row}').value = filtered_src_cm_df.values.tolist()
-            tgt_wb.close()
+            tgt_wb.save()
         except Exception as e:
             log_error(error_log,f"处理{code_map_file}中{table_name} 表的代码映射时发生错误:{str(e)}.")
-            #tgt_wb.close()
+        finally:
+            src_wb.close()
+            tgt_wb.save()
+            tgt_wb.close()
         processed_table_count = processed_table_count + 1
         print(f"剩余{table_list-processed_table_count}个")
     tgt_wb.save(target_file)
