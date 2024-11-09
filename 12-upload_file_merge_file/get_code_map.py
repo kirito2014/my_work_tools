a4f5b9ca4b46f709b0a2264fe7fc8c11f1c19420
kirito2014 <1039580151@qq.com>
2024-11-09 20:04:57 +0800

    refactor: move get_code_map.py, update paths and fix typos in 12-upload_file_merge_file/

--- 12-upload_file_merge_file/get_code_map.py
+++ 12-upload_file_merge_file/get_code_map.py
@@ -8,8 +8,9 @@ def process_code_mapping(table_list_file):
     excel_app = xw.App(visible=False) 
     person_name=os.path.splitext(os.path.basename(table_list_file))[0].split('-')[-1] 
 
-    error_log=f"error_log{person_name}.txt" 
-    target_file='pub_cd_map.xlsx'
+    error_log=f"error_log_{person_name}.txt" 
+    #output目录拼接
+    target_file=os.path.join('output','outpub_cd_map.xlsx')
     
     if os.path.exists(target_file):
         tgt_wb=xw.Book(target_file) 
@@ -24,23 +25,25 @@ def process_code_mapping(table_list_file):
     processed_table_count=0 
     
     for table_name in table_names: 
-        print(f"正在处理<{person_name}>-<{table name}>的码值映射。") 
-        code_map_files=[ 
-            os.path.join('pub_cd map',f'pub_cd_map-(person_name).xlsx'), 
-            os.path.join('pub_cd_map',f'pub_cd_map-(person_name).xls'), 
-            os.path.join('pub_cd_map',f'pub_cd_map-(person_name).xlsm') 
+        print(f"正在处理<{person_name}>-<{table_name}>的码值映射。") 
+        code_map_files = [ 
+            os.path.join('uploads',f'pub_cd_map-{person_name}.xlsx'), 
+            os.path.join('uploads',f'pub_cd_map-{person_name}.xls'), 
+            os.path.join('uploads',f'pub_cd_map-{person_name}.xlsm') 
             ]
-        code_map_file - next((file for file in code_map_files if os.path.exists(file)),None)
+        code_map_file = next((file for file in code_map_files if os.path.exists(file)),None)
+        print(code_map_files)
         if not code_map_file: 
-            1og_error(error_log,f"{person_name}的代码映射文件不存在.") 
+            log_error(error_log,f"{person_name}的代码映射文件不存在.") 
             sys.exit() 
         try: 
             src_wb=xw.Book(code_map_file)
             rem_code_map_sheet='rem-代码映射' 
             if rem_code_map_sheet not in [sheet.name for sheet in src_wb.sheets]: 
                 log_error(error_log,f"{code_map_file}中未找到代码映射sheet。") 
-                src_wb.close() 
-                sys.exitO 
+                #src_wb.close() 
+                sys.exit()
+
             src_cm_sheet=src_wb.sheets[rem_code_map_sheet] 
             if rem_code_map_sheet not in [sheet.name for sheet in tgt_wb.sheets]: 
                 tgt_wb.sheets.add(rem_code_map_sheet) 
@@ -49,26 +52,25 @@ def process_code_mapping(table_list_file):
             #先删除目标文件中已存在的码值映射
             tgt_cm_data=tgt_cm_sheet.range('A1').expand('table').value 
             if tgt_cm_data: 
-                tgt_cm_df=pd.DataFrame(tgt_cm_data[1:],columns-tgt_cm_data[1]) 
+                tgt_cm_df=pd.DataFrame(tgt_cm_data[1:],columns=tgt_cm_data[1]) 
                 tgt_cm_df=tgt_cm_df[tgt_cm_df['目标表英文名'] != table_name] 
-                tgt cm sheet.clear_contents()
-                tgt_cm sheet.range('A1').value = [tgt_cn_data[0]] + tgt_cm_df.values.tolist()
+                tgt_cm_sheet.clear_contents()
+                tgt_cm_sheet.range('A1').value = [tgt_cm_data[0]] + tgt_cm_df.values.tolist()
             #读取并筛选源文件中的码值映射数据
-            src_cm_data-src_cm_sheet.range('A1').expand('table').value 
+            src_cm_data=src_cm_sheet.range('A1').expand('table').value 
             if src_cm_data: 
                 src_cm_df =pd.DataFrame(src_cm_data[1:],columns=src_cm_data[1]) 
                 filtered_src_cm_df=src_cm_df[src_cm_df['目标表英文名']==table_name] 
                 if filtered_src_cm_df.empty: 
-                    log_error(error_log,f"{code_map_file}中未找到{table name}表的代码映射.")
+                    log_error(error_log,f"{code_map_file}中未找到{table_name}表的代码映射.")
+                    continue
                 else:
                     start_row = tgt_cm_sheet.range('A1').expand('down').last_cell.row + 1
                     tgt_cm_sheet.range(f'A{start_row}').value = filtered_src_cm_df.values.tolist()
             tgt_wb.close()
         except Exception as e:
             log_error(error_log,f"处理{code_map_file}中{table_name} 表的代码映射时发生错误:{str(e)}.")
-        finally:
-            tgt_wb.save()
-            tgt_wb.close()
+            #tgt_wb.close()
         processed_table_count = processed_table_count + 1
         print(f"剩余{table_list-processed_table_count}个")
     tgt_wb.save(target_file)
@@ -82,6 +84,6 @@ if __name__ == "__main__":
         print('11111')
         sys.exit(1)
     table_list_file = sys.argv[1]
-    process_code_mapping(table_list_file)
+    process_code_mapping(os.path.join('uploads',table_list_file))
             
             
