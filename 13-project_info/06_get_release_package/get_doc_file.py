8731746a2fa71d7330e220ff0dddfd1aadd4f37b
kirito2014 <1039580151@qq.com>
2025-03-03 14:56:41 +0800

    refactor(项目信息): 优化 get_doc_file.py 脚本功能
    - 增加日志记录功能
    - 修改特殊文件处理流程，备份建表语句
    - 优化错误提示信息，增加上线清单配置检查提示
    - 更新文件头注释，增加版本号和修改历史信息

--- 13-project_info/06_get_release_package/get_doc_file.py
+++ 13-project_info/06_get_release_package/get_doc_file.py
@@ -1,3 +1,22 @@
+"""
+文件名(File Name)               :get_doc_file.py
+作者(Author)                    :wangmujun@Sunline
+编写时间(CreateTime)            :2024-10-01
+版本号(Version)                 :V1.1.0
+使用方法(Usage)                 :python get_doc_files.py <上线包路径> <上线清单>
+功能描述(Descriptions)          :
+    本脚本主要用于实现以下功能:
+    1、根据提供的清单文件自动拾取对应的上线文件
+    2、
+依赖库(Dependences):
+    - 
+修改历史(Histories):
+    v1.0.0 - 2024-10-01 - 初始版本
+    v1.1.0 - 2024-10-01 - 增加日志记录;修改清单模板,增加配置文件中特殊处理的操作
+    
+"""
+
+
 import os
 import shutil
 import sys
@@ -105,7 +124,7 @@ def copy_all_files(base_dir, backup_dir, table_job_list):
 
 def delete_special_files(backup_dir, table_job_list):
     logger.info('----------------- 删除特殊文件 -------------')
-    special_dir = os.path.join(os.path.dirname(backup_dir), 'special_tables')
+    special_dir = os.path.join(os.path.dirname(backup_dir), '建表语句备份')
     if not os.path.exists(special_dir):
         os.makedirs(special_dir)
 
@@ -118,7 +137,7 @@ def delete_special_files(backup_dir, table_job_list):
             if len(create_table_files) == 1:
                 create_table_file = os.path.join(backup_create_table_dir, create_table_files[0])
                 special_table_file = os.path.join(special_dir, create_table_files[0])
-                shutil.copy2(create_table_file, special_table_file)  # 备份到 special_tables
+                shutil.copy2(create_table_file, special_table_file)  # 备份建表语句
                 os.remove(create_table_file)  # 删除 agl-package-new 中的文件
                 logger.info(f"特殊处理: {table_name} 的建表文件已备份到 {special_table_file} 并删除 {create_table_file}.")
             elif len(create_table_files) > 1:
@@ -147,7 +166,8 @@ def main(list_file, base_dir):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        logger.error("Usage: python get_doc_files.py &lt;上线包路径&gt; &lt;上线清单&gt;")
+        logger.error("参数错误, 请使用以下命令: python get_doc_files.py <上线包路径> <上线清单> ")
+        logger.error("执行前请检查上线清单是否配置正确 以 英文表名|作业编号|开发人员|是否特殊处理(Y/N) 的格式填写")
         sys.exit(1)
     base_dir = sys.argv[1]
     list_file = sys.argv[2]
