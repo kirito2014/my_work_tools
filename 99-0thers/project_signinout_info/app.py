481f50a007cb1cb66bc9dc7c7e0e0eff8e56df7d
kirito2014 <1039580151@qq.com>
2025-07-18 08:39:29 +0800

    fix: 移除数据库连接的SSL配置以解决连接问题

--- 99-0thers/project_signinout_info/app.py
+++ 99-0thers/project_signinout_info/app.py
@@ -20,7 +20,7 @@ def get_db_connection():
         database=DB_CONFIG['database'],
         port=DB_CONFIG['port'],
         charset=DB_CONFIG['charset'],
-        ssl={'ca': '/path/to/ca.pem'},
+        #ssl={'ca': '/path/to/ca.pem'},
         allow_public_key=True,
         cursorclass=pymysql.cursors.DictCursor
     )
